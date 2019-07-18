ec87f6249f525069a47b8639284a33c6f95d40e0
LULI3 <luanyuanshizhu@126.com>
2019-07-18 20:39:38 +0800

    create and test update the order when car is live

--- src/test/java/com/thoughtworks/parking_lot/repository/ParkingOrderRepositoryTest.java
+++ src/test/java/com/thoughtworks/parking_lot/repository/ParkingOrderRepositoryTest.java
@@ -14,6 +14,7 @@ import java.util.Date;
 import java.sql.Timestamp;
 import java.util.Optional;
 
+import static java.lang.Thread.sleep;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
@@ -25,12 +26,26 @@ public class ParkingOrderRepositoryTest {
     ParkingOrderRepository parkingOrderRepository;
 
     @Test
-    public void return_add_and_return_a_new_order(){
+    public void should_add_and_return_a_new_order(){
         ParkingOrder parkingOrder = new ParkingOrder("13245", new Timestamp(new Date().getTime()),null, "open", new ParkingLot("第一停车场",2,"香洲区"));
 
         ParkingOrder parkingOrder1 = parkingOrderRepository.save(parkingOrder);
 
         assertThat(parkingOrder1.getCarId()).isEqualTo("13245");
+    }
+
+    @Test
+    public void should_update_the_order_when_car_is_live() throws InterruptedException {
+        ParkingOrder parkingOrder = new ParkingOrder("13245", new Timestamp(new Date().getTime()),null, "open", new ParkingLot("第一停车场",2,"香洲区"));
+
+        sleep(1000);
+        ParkingOrder parkingOrder1 = parkingOrderRepository.save(parkingOrder);
+        parkingOrder1.setCarOutTime(new Timestamp(new Date().getTime()));
+        parkingOrder1.setOrderStatus("close");
+
+        ParkingOrder returnParkingOrder = parkingOrderRepository.save(parkingOrder1);
+
+        assertThat(returnParkingOrder.getOrderStatus()).isEqualTo("close");
     }
 
 }
