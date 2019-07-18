e53c1b8b09bc8aa07e4ea2d3aa4f64b538447400
LULI3 <luanyuanshizhu@126.com>
2019-07-18 18:15:42 +0800

    test delete parkingLot by id

--- src/test/java/com/thoughtworks/parking_lot/repository/ParkingLotRepositoryTest.java
+++ src/test/java/com/thoughtworks/parking_lot/repository/ParkingLotRepositoryTest.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
@@ -27,5 +29,16 @@ public class ParkingLotRepositoryTest {
         assertThat(returnParkingLot.getCapacity()).isEqualTo(100);
         assertThat(returnParkingLot.getPosition()).isEqualTo("香洲区");
 
+    }
+
+    @Test
+    public void should_delete_parkingLot(){
+        ParkingLot parkingLot = new ParkingLot(100,"香洲区");
+
+        ParkingLot parkingLot1 = parkingLotRepository.save(parkingLot);
+        parkingLotRepository.delete(parkingLot);
+        Optional<ParkingLot> returnParkingLot = parkingLotRepository.findById(parkingLot1.getId());
+
+        assertThat(returnParkingLot.equals(null));
     }
 }
